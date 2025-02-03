94b2f7a6f3aa795af6e41c5bbc2986377205cde1
antholim <139510082+antholim@users.noreply.github.com>
2025-02-02 20:52:09 -0500

    LINKUP-9.1 : Implement React router architecture

---
+++ src/Frontend/src/pages/Login/LoginPage.jsx
@@ -0,0 +1,9 @@
+import LoginComponent from "./LoginComponent";
+
+function LoginPage() {
+    return (
+        <>
+            <LoginComponent/>
+        </>
+    )
+} export default LoginPage;
---
+++ src/Frontend/src/pages/Register/RegisterPage.jsx
@@ -0,0 +1,9 @@
+import RegisterComponent from "./RegisterComponent";
+
+function RegisterPage() {
+    return (
+        <>
+            <RegisterComponent />
+        </>
+    )
+} export default RegisterPage;
