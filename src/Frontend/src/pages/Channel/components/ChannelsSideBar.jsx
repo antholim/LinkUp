da47a48b9c1e2f95c9a03d73ab027debad4d603e
antholim <anthonylim459@hotmail.com>
2025-02-21 12:41:35 -0500

    LINKUP-6 : Create channel feature in frontend (Task : 6.3)

--- src/Frontend/src/pages/Channel/components/ChannelsSideBar.jsx
+++ src/Frontend/src/pages/Channel/components/ChannelsSideBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { categories } from '../../../data/FakeChannels';
+import { useState, useRef } from 'react';
 
+let plus = '+';
+let minus = '-';
 const ChannelsSidebar = ({ serverName, activeChannel, onChannelSelect }) => {
-
+    const [creatingChannel, setCreatingChannel] = useState(false);
+    const inputRef = useRef(null);
+    const handleCreateChannel = () => {
+        categories[0].channels.push({ id: categories[0].channels.length, name: inputRef.current.value, icon: '#' });
+        setCreatingChannel(false);
+    }
     return (
         <div className="channels-sidebar">
             <div className="server-header">
@@ -12,6 +20,9 @@ const ChannelsSidebar = ({ serverName, activeChannel, onChannelSelect }) => {
                 <div key={category.name}>
                     <div className="channels-category">
                         {category.name}
+                        <button className='create-channel-button' onClick={()=> {
+                            setCreatingChannel(()=> !creatingChannel);
+                        }}>{creatingChannel ? minus : plus}</button>
                     </div>
                     <div className="channel-list">
                         {category.channels.map(channel => (
@@ -24,6 +35,11 @@ const ChannelsSidebar = ({ serverName, activeChannel, onChannelSelect }) => {
                                 {channel.name}
                             </div>
                         ))}
+                        
+                        {creatingChannel && <>
+                            <input ref={inputRef} className='input-channel'/>
+                            <button onClick={handleCreateChannel}>Create</button>
+                        </>}
                     </div>
                 </div>
             ))}
